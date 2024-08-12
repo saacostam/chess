276cd6c6b5489046c9a62ea3b3e0f950cf313ca7
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2024-08-11 19:23:17 -0500

    style: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { ChessBoard, ChessPieceColor } from './modules/chess-board';
 function App() {
   return (
     <>
-      <h1 className='text-4xl text-accent font-bold my-8 text-center'>Chess | saacostam</h1>
+      <h1 className="text-4xl text-accent font-bold my-8 text-center">
+        Chess | saacostam
+      </h1>
       <ChessBoard currentPlayerColor={ChessPieceColor.WHITE} />
     </>
   );
